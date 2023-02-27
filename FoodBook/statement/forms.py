4d11d7700592f4624eca3b70e7bf786f9b743874
beatriznisilva <beatriznisilva28@gmail.com>
2023-02-27 19:39:51 +0000

    3

--- FoodBook/statement/forms.py
+++ FoodBook/statement/forms.py
@@ -6,4 +6,3 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
-
